e2750cb79df39177f4d4b58b9ab34452f714eef7
Kfir <kggold4@gmail.com>
2022-05-22 09:41:54 +0300

    seperate to two notebooks

--- utli_functions.py
+++ utli_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib_inline as plt
+import matplotlib.pyplot as plt
+
 
 def create_data_from_file(file_name: str):
     data = []
@@ -12,22 +13,34 @@ def create_data_from_file(file_name: str):
     label = np.array(label)
     return np.array(data), np.array(label)
 
+
 def plot_labels(x, model):
     """
     Plot the outputs of each neuron in the given layer
     """
-    neuron_number = 0
-
     # for neuron in layer_output:
     y_hat=model.predict(x)
     # green dots
-    plt.scatter(x=x[y_hat == -1, 1], y=x[y_hat == -1, 0], alpha=0.9,
-                    c='green', marker='s', label=-1.0)
+    plt.scatter(x=x[y_hat == -1, 1], y=x[y_hat == -1, 0], alpha=0.9, c='green', marker='s', label=-1.0)
 
     # orange dots
-    plt.scatter(x=x[y_hat == 1, 1], y=x[y_hat == 1, 0], alpha=0.9,
-                    c='orange', label=1.0)
+    plt.scatter(x=x[y_hat == 1, 1], y=x[y_hat == 1, 0], alpha=0.9, c='orange', label=1.0)
 
     # location of the legend
     plt.legend(loc='upper left')
     plt.show()
+
+
+def mse(real, output) -> float:
+    """
+    Calculate the mean square error of predicted label data and the real label data
+    """
+    return ((np.array(real) - np.array(output))**2).mean()
+
+
+def accuracy(real, output) -> float:
+    """
+    Calcukate the accuracy of predicted label data and the real label data
+    """
+    count = t != np.sign(output)
+    return np.sum(count)/len(real)
